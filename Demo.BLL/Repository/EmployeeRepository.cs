c7e621132a3eb989d2a6781ce3598f3512251550
pc <pc@DESKTOP-2MO1OGA>
2024-03-20 23:25:14 +0200

    BusinessLogicLayer

---
+++ Demo.BLL/Repository/EmployeeRepository.cs
@@ -0,0 +1,53 @@
+﻿using Demo.BLL.Interfaces;
+using Demo.DAL.Data;
+using Demo.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.BLL.Repository
+{
+    internal class EmployeeRepository : IEmployeeRepository
+    {
+
+        private MVCAPP_DbContext dbContext;
+
+        public EmployeeRepository(MVCAPP_DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            //dbContext = new MVCAPP_DbContext();
+        }
+        public int Add(Employee employee)
+        {
+            dbContext.Add(employee);
+           
+            return dbContext.SaveChanges();
+        }
+
+        public int Delete(Employee employee)
+        {
+           dbContext.Remove(employee);
+           return dbContext.SaveChanges();
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return dbContext.Employees.ToList();
+        }
+
+        public Employee GetById(int id)
+        {
+           return dbContext.Employees.Where(E => E.EmployeeId == id).FirstOrDefault();
+        }
+
+        public int Update(Employee employee)
+        {
+           dbContext.Update(employee);
+
+            return dbContext.SaveChanges();
+        }
+    }
+}
--- Demo.DAL/Data/MVCAPP_DbContext.cs
+++ Demo.DAL/Data/MVCAPP_DbContext.cs
@@ -8,15 +8,20 @@ using System.Threading.Tasks;
 
 namespace Demo.DAL.Data
 {
-    internal class MVCAPP_DbContext : DbContext
+    public class MVCAPP_DbContext : DbContext
     {
+
+        public MVCAPP_DbContext(DbContextOptions<MVCAPP_DbContext> options) : base(options)
+        {
+            
+        }
         public DbSet<Employee> Employees { get; set; }
 
         public DbSet<Department> Departments { get; set; }
 
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer("Server = . ; Database = MVC_APP ; Trusted_Connection = true ; Encrypted = false;"); // EF Core
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //=> optionsBuilder.UseSqlServer("Server = . ; Database = MVC_APP ; Trusted_Connection = true ; Encrypted = false;"); // EF Core
 
     }
 }
