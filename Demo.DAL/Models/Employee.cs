6e23cb7bf521147cc9be88cd9641afbaaafe149f
pc <pc@DESKTOP-2MO1OGA>
2024-03-20 22:04:53 +0200

    DataAccessLayer

---
+++ Demo.DAL/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Models
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is Required")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public double Salary { get; set; }
+    }
+}
