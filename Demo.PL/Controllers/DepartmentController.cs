0d91a352231386ed0b262472ae833ed358c41b5b
pc <pc@DESKTOP-2MO1OGA>
2024-03-21 14:58:17 +0200

    Dependency Injection

---
+++ Demo.PL/Controllers/DepartmentController.cs
@@ -0,0 +1,19 @@
+﻿using Demo.BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.PL.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private IDepartmentRepository _departmentRepository;
+
+        public DepartmentController(IDepartmentRepository departRepository)
+        {
+            _departmentRepository = departRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Demo.PL/Startup.cs
+++ Demo.PL/Startup.cs
@@ -1,3 +1,5 @@
+using Demo.BLL.Interfaces;
+using Demo.BLL.Repository;
 using Demo.DAL.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -31,6 +33,8 @@ namespace Demo.PL
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
 
             }); //allow dependency injection
+
+            services.AddScoped<IDepartmentRepository , DepartmentRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
