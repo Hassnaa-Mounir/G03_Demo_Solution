9b85aec34d5d46a4ab68107a8f6eda58e39585a9
pc <pc@DESKTOP-2MO1OGA>
2024-03-26 15:41:53 +0200

    UpdateDALofEmployee

---
+++ Demo.DAL/Migrations/20240326000716_UpdateEmployeeData01.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Demo.DAL.Migrations
+{
+    public partial class UpdateEmployeeData01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Gender",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Gender",
+                table: "Employees",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
--- Demo.DAL/Models/Employee.cs
+++ Demo.DAL/Models/Employee.cs
@@ -2,21 +2,40 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Enum;
 
 namespace Demo.DAL.Models
 {
+    
     public class Employee
     {
-        public int EmployeeId { get; set; }
+       
+        public int Id{ get; set; }
 
         [Required(ErrorMessage = "Name is Required")]
-        [MaxLength(50)]
+        [MaxLength(50,ErrorMessage ="Maximum Length of name 50")]
+        [MinLength(5, ErrorMessage = "Minimum Length of name 50")]
         public string Name { get; set; }
 
+        //[RegularExpression("^[0-9]{1-3}-[a-zA-Z]{4-10}-[a-zA-Z]{3-10}-[a-zA-Z]{4-10}",ErrorMessage ="Address Must That As 123-Street-City-Country")]
         public string Address { get; set; }
 
-        public double Salary { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal Salary { get; set; }
+
+        public bool IsActive { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+        public int Gender { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; } 
+
     }
 }
