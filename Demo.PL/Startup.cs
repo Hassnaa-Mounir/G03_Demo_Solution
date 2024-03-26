44a822155c408837b071eb395006cb4491effeda
pc <pc@DESKTOP-2MO1OGA>
2024-03-26 15:44:14 +0200

    ViewsOfEmployee

--- Demo.PL/Startup.cs
+++ Demo.PL/Startup.cs
@@ -35,6 +35,8 @@ namespace Demo.PL
             }); //allow dependency injection
 
             services.AddScoped<IDepartmentRepository , DepartmentRepository>();
+
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
